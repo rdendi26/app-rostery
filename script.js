5f2bd8c111f8e850e6f56762a8d93b6350e032c0
rdendi <rdendi2693@gmail.com>
2024-04-30 13:47:54 +0700

    perubahan baru

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const form = document.querySelector('.dencity');
+const suhu = document.querySelector(".p1");
+const reqSuhu = document.querySelector(".p2");
+
+function setTextConten (dencity){
+    suhu.textContent = `densitas : ${dencity}`;
+    if(dencity < 650 ){
+        reqSuhu.textContent = `Suhu yang di rekomendasikan di 150`
+
+    }else if ( dencity >= 651 && dencity < 700 ){
+        reqSuhu.textContent = `Suhu yang di rekomendasikan di 160`
+
+    }else if (dencity >= 701 && dencity < 750 ){
+        reqSuhu.textContent = `Suhu yang di rekomendasikan di 170`
+
+    }else if ( dencity > 750 ){
+        reqSuhu.textContent = `Suhu yang di rekomendasikan di 190`
+
+    }else{
+        reqSuhu.textContent = `Suhu di luar batas`
+
+    }
+};
+
+
+
+
 
 form.addEventListener('submit',(e) => {
     // mematikan refres
@@ -11,8 +37,10 @@ form.addEventListener('submit',(e) => {
     let hasil = gram / milli;
     let dencity = hasil * 1000;
 
+    
 
-    console.log(dencity);
-    // console.log(milli + ' milli');
+    setTextConten(dencity);
+
+    
 
 });
